650f7c363c0c74515133c7c9015aee7391a76ca3
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-05-30 20:28:32 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ let colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
 colorbit_51bit.clear()
 colorbit_51bit.setBrightness(32)
 colorbit_51bit.showColorIcon(ColorIcon.FillFull, colorbit.colors(BitColors.Blue))
-basic.pause(200)
+basic.pause(500)
 basic.forever(function () {
     colorbit_51bit.drawColorBit(randint(0, 4), randint(0, 4), colorbit.colors(BitColors.Black))
     basic.pause(randint(16, 24))
