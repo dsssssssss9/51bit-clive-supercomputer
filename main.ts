2a8bdcf22d21ba39ddbae3dc9799ec6a5843b612
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-05-31 17:27:41 +0100

    Added Scrolling intro message

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 let colorbit_51bit = colorbit.initColorBit(DigitalPin.P1, BitColorMode.RGB)
 colorbit_51bit.clear()
-colorbit_51bit.setBrightness(32)
+colorbit_51bit.setBrightness(16)
+colorbit_51bit.showScrollStringColor("Big Clive SuperComputer", colorbit.colors(BitColors.Red))
+basic.pause(500)
 colorbit_51bit.showColorIcon(ColorIcon.FillFull, colorbit.colors(BitColors.Blue))
 basic.pause(500)
 basic.forever(function () {
